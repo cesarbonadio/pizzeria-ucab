8a3fcaaf40d30af80a9310461eda852258bff5d0
cesarbonadio <cesarbonadio123@gmail.com>
2019-10-23 13:03:43 -0400

    cli messages separated

---
+++ util/cliMsg.py
@@ -0,0 +1,17 @@
+#Archivo para mantener separadas constantes de mensajes que se muestran por terminal y no sobrecargar codigo en otro lado
+
+welcomeTitle = "\n\n***************\n*PIZZERIA UCAB*\n***************"
+zoneQuestion = "\n¿Quiere separar los toppings de la pizza por areas?[s/n]:"
+zoneOptions = "Seleccionar la cantidad de zonas en las que se dividirá la pizza (max 4):"
+ingredientOptionsInstructive = "\nA continuación se le presentan la lista de los ingredientes: \n" \
+                                "Presione enter si quiere dejar de introducir ingrediente \n" \
+                                "Escriba '-' antes del ingrediente si quiere eliminar un ingrediente. Ej: -ja \n" \
+                                "El precio de los igredientes se reducen dependiendo del tamaño de la zona \n"
+ingredientNotDeleted = "No se eliminó el ingrediente. No está agregado a la pizza o no existe"
+ingredientDeleted = "Una ración de ingrediente eliminado: "
+ingredientNotAdded = "No se pudo agregar el ingrediente porque no existe"
+ingredientAdded = "Ingrediente Agregado de forma exitosa"
+sizeSelected = "\nTamaño seleccionado: "
+sizeSelectionError = "\n\nPorfavor indique un tamaño válido"
+continueQuestion = "\n¿Desea continuar (elegir otra pizza)?[s/n]:"
+inputIngredient = "Indique el ingrediente (enter para terminar):"
